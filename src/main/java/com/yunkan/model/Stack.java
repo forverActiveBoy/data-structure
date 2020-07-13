1236b3269795941b085460707b17e4ccb9f0c0c0
foreverActiveBoy <extends_zkx0924@163.com>
2020-07-13 20:41:54 +0800

    V1.0.0-栈接口

---
+++ src/main/java/com/yunkan/model/Stack.java
@@ -0,0 +1,38 @@
+package com.yunkan.model;
+
+/**
+ * @author foreverActiveBoy
+ * @date 2020/7/13 20:17
+ * @apiNote 栈
+ */
+public interface Stack<E> {
+    /**
+     * 获取栈中元素个数
+     * @return
+     */
+    int getSize();
+
+    /**
+     * 判断栈是不是为空
+     * @return
+     */
+    boolean isEmpty();
+
+    /**
+     * 入栈
+     * @param e
+     */
+    void push(E e);
+
+    /**
+     * 出栈
+     * @return
+     */
+    E pop();
+
+    /**
+     * 查看栈顶元素
+     * @return
+     */
+    E peek();
+}
